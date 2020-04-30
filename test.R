abf96e336f4d2b21aeebdac406535b9b67875093
Yves Reecht <Yves.Reecht@hi.no>
2020-04-30 15:55:34 +0200

    dkjfbal

--- test.R
+++ test.R
@@ -1,16 +1,5 @@
 ls()
 dir()
 
-## Wanted change 1
-getwd()
-ls("..")
-
-## I shouldn't have suppressed the previous informative comment!!!
-message("Bla")
-
-## Unwanted addition!
-
-## Wanted!
-
 
 
